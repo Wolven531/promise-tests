172e99c63cd693955e5f0fab079b7840af47aa44
Anthony Williams <anthony.williams.cs@gmail.com>
2019-06-04 21:25:05 -0600

    Add code for promise rejection

--- src/App.tsx
+++ src/App.tsx
@@ -5,19 +5,28 @@ import logo from './logo.svg'
 
 import './App.css'
 
+interface IResolution {
+	error?: string
+	msg: string
+}
+
 const App: React.FC = () => {
 	useEffect(() => {
 		const token = '[useEffect | App]'
 		console.info(`${token} mounting...`)
 
 		const p: Promise<any> = new Promise((resolve, reject) => { resolve({ msg: 'from resolve' }) })
+		// const p: Promise<any> = new Promise((resolve, reject) => { reject({ error: 'from reject' }) })
 
 		console.info(`${token} promise created`)
 
-		p.then(resolution => {
+		p.then((resolution: IResolution) => {
+			if (resolution.error) {
+				throw new Error(resolution.error)
+			}
 			console.info(`${token} inside then... resolution.msg=${resolution.msg}...`)
 		}).catch(err => {
-			console.error(`${token} an error occured...`, err)
+			console.error(`${token} an error occured...`, JSON.stringify(err, null, 4))
 		})
 	}, [])
 
