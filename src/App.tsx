d5b1f85d7dcf993a933ae1ba25c53259d7cc7cc6
Anthony Williams <anthony.williams.cs@gmail.com>
2019-06-04 21:41:18 -0600

    Add valid and invalid promise examples w/ notes

--- src/App.tsx
+++ src/App.tsx
@@ -15,8 +15,28 @@ const App: FC = () => {
 		const token = '[useEffect | App]'
 		console.info(`${token} mounting...`)
 
+		// NOTE: VALID way to create resolved promise
 		const p: Promise<IResolution> = new Promise((resolve, reject) => { resolve({ msg: 'from resolve' }) })
+		//
+		// Equivalent to (because reject is unused, and single arg funcs require no parens):
+		// const p: Promise<IResolution> = new Promise(resolve => { resolve({ msg: 'from resolve' }) })
+
+		// NOTE: VALID way to create rejected promise
 		// const p: Promise<IResolution> = new Promise((resolve, reject) => { reject({ error: 'from reject' }) })
+
+		// NOTE: invalid because of TSC: Argument of type '5' is not assignable to parameter of type 'IResolution | PromiseLike<IResolution> | undefined'
+		// const p: Promise<IResolution> = new Promise((resolve, reject) => { resolve(5) })
+
+		// NOTE: is NOT invalid because return type allows undefined
+		// WARNING: does NOT match expected return type on resolution
+		// const p: Promise<IResolution> = new Promise((resolve, reject) => { resolve() })
+
+		// NOTE: is NOT invalid because return type allows undefined
+		// WARNING: leaves promise unresolved AND unrejected - causes problems
+		// const p: Promise<IResolution> = new Promise((resolve, reject) => { return })
+		//
+		// Equivalent to
+		// const p: Promise<IResolution> = new Promise((resolve, reject) => {})
 
 		console.info(`${token} promise created`)
 
