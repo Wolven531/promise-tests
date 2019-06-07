29bb3ad4821fe6b449ad521ef3c3d22b90bbacdc
Anthony Williams <anthony.williams.cs@gmail.com>
2019-06-06 22:40:07 -0600

    Use new AppWithAsync

--- src/index.tsx
+++ src/index.tsx
@@ -2,14 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 // import App from './App'
-import AppWithQ from './AppWithQ'
+import AppWithAsync from './AppWithAsync'
+// import AppWithQ from './AppWithQ'
 
 import * as serviceWorker from './serviceWorker'
 
 import './index.css'
 
 // ReactDOM.render(<App />, document.getElementById('root'))
-ReactDOM.render(<AppWithQ />, document.getElementById('root'))
+ReactDOM.render(<AppWithAsync />, document.getElementById('root'))
+// ReactDOM.render(<AppWithQ />, document.getElementById('root'))
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
