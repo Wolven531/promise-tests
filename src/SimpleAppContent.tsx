9fb461b5616880a665a4e547d666353354a8b67d
Anthony Williams <anthony.williams.cs@gmail.com>
2020-01-02 22:14:00 -0700

    Enable SImpleAppContent to set page title

--- src/SimpleAppContent.tsx
+++ src/SimpleAppContent.tsx
@@ -1,13 +1,25 @@
-import React, { FC } from 'react'
+import React, { FC, useEffect } from 'react'
+
+// utils
+import { setPageTitle } from './utils'
 
 // resources
 import logo from './logo.svg'
 
-const SimpleAppContent: FC<any> = (props) => {
+interface ISimpleAppProps {
+	pageTitle: string
+}
+
+const SimpleAppContent: FC<ISimpleAppProps> = (props) => {
+	useEffect(() => {
+		setPageTitle(props.pageTitle)
+	}, [])
+
 	return (
 		<div className="app">
 			<header className="app-header">
 				<img src={logo} className="app-logo" alt="logo" />
+				<h1>{props.pageTitle}</h1>
 			</header>
 		</div>
 	)
