8f4b1cfe91acfb06c86d56d723a4a297594191a6
Anthony Williams <anthony.williams.cs@gmail.com>
2020-01-02 22:16:16 -0700

    Ensure SimpleAppContent useEffect() watches props

--- src/SimpleAppContent.tsx
+++ src/SimpleAppContent.tsx
@@ -13,13 +13,14 @@ interface ISimpleAppProps {
 const SimpleAppContent: FC<ISimpleAppProps> = (props) => {
 	useEffect(() => {
 		setPageTitle(props.pageTitle)
-	}, [])
+	}, [props.pageTitle])
 
 	return (
 		<div className="app">
 			<header className="app-header">
 				<img src={logo} className="app-logo" alt="logo" />
 				<h1>{props.pageTitle}</h1>
+				<p>Hit F12 to open the developer console and see how the Promise executed</p>
 			</header>
 		</div>
 	)
