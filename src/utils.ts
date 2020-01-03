e9c075c45e5c69e1c7a8889d18899d02c645800f
Anthony Williams <anthony.williams.cs@gmail.com>
2020-01-02 22:07:12 -0700

    Add utils file

---
+++ src/utils.ts
@@ -0,0 +1,10 @@
+/**
+ *
+ * @param newTitle The value that will become the page title
+ * @type string
+ */
+const setPageTitle = (newTitle: string) => {
+	window.document.title = newTitle
+}
+
+export { setPageTitle }
