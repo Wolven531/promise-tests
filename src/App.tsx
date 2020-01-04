f3e5dfc629c45336ca54f2158e56a7353e055c0f
Anthony Williams <anthony.williams.cs@gmail.com>
2020-01-03 21:22:56 -0700

    Allow cycling through examples

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,40 @@
-import React, { FC, useEffect } from 'react'
+import React, { FC, useEffect, useState } from 'react'
 
 // utils
 import { IResolution } from './utils'
 
 // local
 import {
-	EXAMPLE_VALID_NATIVE_PROMISE
+	EXAMPLE_INVALID_NATIVE_PROMISE,
+	EXAMPLE_INVALID_NATIVE_PROMISE_UNDEFINED,
+	EXAMPLE_INVALID_NATIVE_PROMISE_UNRESOLVED,
+	EXAMPLE_INVALID_NATIVE_PROMISE_UNRESOLVED_2,
+	EXAMPLE_VALID_NATIVE_PROMISE,
+	EXAMPLE_VALID_NATIVE_PROMISE_REJECTED,
+	EXAMPLE_VALID_NATIVE_PROMISE_SINGLE_ARG
 } from './examples'
 import { SimpleAppContent } from './SimpleAppContent'
 
 import './App.css'
 
+const examples = [
+	EXAMPLE_VALID_NATIVE_PROMISE,
+	EXAMPLE_VALID_NATIVE_PROMISE_SINGLE_ARG,
+	EXAMPLE_VALID_NATIVE_PROMISE_REJECTED,
+	EXAMPLE_INVALID_NATIVE_PROMISE,
+	EXAMPLE_INVALID_NATIVE_PROMISE_UNDEFINED,
+	EXAMPLE_INVALID_NATIVE_PROMISE_UNRESOLVED,
+	EXAMPLE_INVALID_NATIVE_PROMISE_UNRESOLVED_2
+]
+
 const App: FC<any> = () => {
+	const [currentExampleIndex, setCurrentExampleIndex] = useState(0)
+
 	useEffect(() => {
 		const token = '[useEffect | App]'
 		console.info(`${token} mounting...`)
 
 		const p: Promise<IResolution> = new Promise((resolve, reject) => { resolve({ msg: 'from resolve' }) })
-
-		// NOTE: invalid because of TSC: Argument of type '5' is not assignable to parameter of type 'IResolution | PromiseLike<IResolution> | undefined'
-		// const p: Promise<IResolution> = new Promise((resolve, reject) => { resolve(5) })
-
-		// NOTE: is NOT invalid because return type allows undefined
-		// WARNING: does NOT match expected return type on resolution
-		// const p: Promise<IResolution> = new Promise((resolve, reject) => { resolve() })
-
-		// NOTE: is NOT invalid because return type allows undefined
-		// WARNING: leaves promise unresolved AND unrejected - causes problems
-		// const p: Promise<IResolution> = new Promise((resolve, reject) => { return })
-		//
-		// Equivalent to
-		// const p: Promise<IResolution> = new Promise((resolve, reject) => {})
 
 		console.info(`${token} promise created`)
 
@@ -46,11 +50,28 @@ const App: FC<any> = () => {
 
 	return (
 		<SimpleAppContent pageTitle={'App (Example - using native Promises)'}>
+			<button style={{
+					borderColor: '#333',
+					borderRadius: 15,
+					borderWidth: 1,
+					color: '#333',
+					fontSize: 18,
+					fontWeight: 'bold',
+					height: 50,
+					padding: 5,
+					width: 200
+				}}
+				onClick={() => { setCurrentExampleIndex(oldIndex => (oldIndex + 1) % examples.length) }}
+				>
+				Next Example
+			</button>
+			<h2>Example {currentExampleIndex + 1}</h2>
 			<textarea
 				cols={120}
 				readOnly={true}
-				rows={13}
-				value={EXAMPLE_VALID_NATIVE_PROMISE}
+				rows={15}
+				style={{ marginBottom: 50 }}
+				value={examples[currentExampleIndex]}
 			/>
 		</SimpleAppContent>
 	)
