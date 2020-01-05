5a20584f4b0886fa28ab045db6370c25df7535ca
Anthony Williams <anthony.williams.cs@gmail.com>
2020-01-04 20:54:57 -0700

    Convert console stmts to alerts in examples

--- src/examples.ts
+++ src/examples.ts
@@ -1,45 +1,45 @@
 export const EXAMPLE_VALID_NATIVE_PROMISE =
 `const p: Promise<IResolution> = new Promise((resolve, reject) => { resolve({ msg: 'from resolve' }) })
 
-console.info('promise created')
+alert('promise created')
 
 p.then(({ error, msg }) => {
 	if (error) {
 		throw new Error(error)
 	}
-	console.info(\`inside then... resolution.msg=\${msg}...\`)
+	alert(\`inside then... resolution.msg=\${msg}...\`)
 }).catch(err => {
-	console.error('an error occured...', JSON.stringify(err, null, 4))
+	alert('an error occured...', JSON.stringify(err, null, 4))
 })
 `
 
 export const EXAMPLE_VALID_NATIVE_PROMISE_SINGLE_ARG =
 `const p: Promise<IResolution> = new Promise(resolve => { resolve({ msg: 'from resolve' }) })
 
-console.info('promise created')
+alert('promise created')
 
 p.then(({ error, msg }) => {
 	if (error) {
 		throw new Error(error)
 	}
-	console.info(\`inside then... resolution.msg=\${msg}...\`)
+	alert(\`inside then... resolution.msg=\${msg}...\`)
 }).catch(err => {
-	console.error('an error occured...', JSON.stringify(err, null, 4))
+	alert('an error occured...', JSON.stringify(err, null, 4))
 })
 `
 
 export const EXAMPLE_VALID_NATIVE_PROMISE_REJECTED =
 `const p: Promise<IResolution> = new Promise((resolve, reject) => { reject({ error: 'from reject' }) })
 
-console.info('promise created')
+alert('promise created')
 
 p.then(({ error, msg }) => {
 	if (error) {
 		throw new Error(error)
 	}
-	console.info(\`inside then... resolution.msg=\${msg}...\`)
+	alert(\`inside then... resolution.msg=\${msg}...\`)
 }).catch(err => {
-	console.error('an error occured...', JSON.stringify(err, null, 4))
+	alert('an error occured...', JSON.stringify(err, null, 4))
 })
 `
 
@@ -47,15 +47,15 @@ export const EXAMPLE_INVALID_NATIVE_PROMISE =
 `// NOTE: invalid because of TSC: Argument of type '5' is not assignable to parameter of type 'IResolution | PromiseLike<IResolution> | undefined'
 const p: Promise<IResolution> = new Promise((resolve, reject) => { resolve(5) })
 
-console.info('promise created')
+alert('promise created')
 
 p.then(({ error, msg }) => {
 	if (error) {
 		throw new Error(error)
 	}
-	console.info(\`inside then... resolution.msg=\${msg}...\`)
+	alert(\`inside then... resolution.msg=\${msg}...\`)
 }).catch(err => {
-	console.error('an error occured...', JSON.stringify(err, null, 4))
+	alert('an error occured...', JSON.stringify(err, null, 4))
 })
 `
 
@@ -64,15 +64,15 @@ export const EXAMPLE_INVALID_NATIVE_PROMISE_UNDEFINED =
 // WARNING: does NOT match expected return type on resolution
 const p: Promise<IResolution> = new Promise((resolve, reject) => { resolve() })
 
-console.info('promise created')
+alert('promise created')
 
 p.then(({ error, msg }) => {
 	if (error) {
 		throw new Error(error)
 	}
-	console.info(\`inside then... resolution.msg=\${msg}...\`)
+	alert(\`inside then... resolution.msg=\${msg}...\`)
 }).catch(err => {
-	console.error('an error occured...', JSON.stringify(err, null, 4))
+	alert('an error occured...', JSON.stringify(err, null, 4))
 })
 `
 
@@ -81,15 +81,15 @@ export const EXAMPLE_INVALID_NATIVE_PROMISE_UNRESOLVED =
 // WARNING: leaves promise unresolved AND unrejected - causes problems
 const p: Promise<IResolution> = new Promise((resolve, reject) => { return })
 
-console.info('promise created')
+alert('promise created')
 
 p.then(({ error, msg }) => {
 	if (error) {
 		throw new Error(error)
 	}
-	console.info(\`inside then... resolution.msg=\${msg}...\`)
+	alert(\`inside then... resolution.msg=\${msg}...\`)
 }).catch(err => {
-	console.error('an error occured...', JSON.stringify(err, null, 4))
+	alert('an error occured...', JSON.stringify(err, null, 4))
 })
 `
 
@@ -98,14 +98,14 @@ export const EXAMPLE_INVALID_NATIVE_PROMISE_UNRESOLVED_2 =
 // WARNING: leaves promise unresolved AND unrejected - causes problems
 const p: Promise<IResolution> = new Promise((resolve, reject) => {})
 
-console.info('promise created')
+alert('promise created')
 
 p.then(({ error, msg }) => {
 	if (error) {
 		throw new Error(error)
 	}
-	console.info(\`inside then... resolution.msg=\${msg}...\`)
+	alert(\`inside then... resolution.msg=\${msg}...\`)
 }).catch(err => {
-	console.error('an error occured...', JSON.stringify(err, null, 4))
+	alert('an error occured...', JSON.stringify(err, null, 4))
 })
 `
