7961d7ee22fc8e887580baee3e286883373d9be5
Anthony Williams <anthony.williams.cs@gmail.com>
2020-01-05 19:54:36 -0700

    Update Example model comments

--- src/Example.ts
+++ src/Example.ts
@@ -1,11 +1,8 @@
 class Example {
 	/**
 	 * @param text The snippet of JS that constitutes the example
-	 * @type string
 	 * @param summary A brief summary of the example
-	 * @type string
 	 * @param description An optional detailed explanation of the example
-	 * @type string | undefined
 	 */
 	constructor(
 		public text: string,
