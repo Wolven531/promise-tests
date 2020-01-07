380edc998587b073c17933ea71fabf399dea6dd1
Anthony Williams <anthony.williams.cs@gmail.com>
2020-01-06 19:27:12 -0700

    First pass at adding navigation

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,42 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import {
+	BrowserRouter as Router,
+	Link,
+	Route,
+	Switch
+} from 'react-router-dom'
 
 import App from './App'
-// import AppWithAsync from './AppWithAsync'
-// import AppWithQ from './AppWithQ'
+import AppWithAsync from './AppWithAsync'
+import AppWithQ from './AppWithQ'
 
 import * as serviceWorker from './serviceWorker'
 
 import './index.css'
 
-ReactDOM.render(<App />, document.getElementById('root'))
-// ReactDOM.render(<AppWithAsync />, document.getElementById('root'))
-// ReactDOM.render(<AppWithQ />, document.getElementById('root'))
+ReactDOM.render((
+	<Router>
+		<Switch>
+			<Route path="/q" component={AppWithQ} />
+			<Route path="/async" component={AppWithAsync} />
+			<Route path="/" component={App} />
+		</Switch>
+		<nav>
+			<ul>
+				<li>
+					<Link to="/">Home (Native Promises)</Link>
+				</li>
+				<li>
+					<Link to="/async">Async / Await Promises</Link>
+				</li>
+				<li>
+					<Link to="/q">Q Library Promises</Link>
+				</li>
+			</ul>
+		</nav>
+	</Router>
+), document.getElementById('root'))
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
