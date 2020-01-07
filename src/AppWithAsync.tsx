bdbf20ca42475b48926505b947bd0260ab2bcced
Anthony Williams <anthony.williams.cs@gmail.com>
2020-01-06 19:00:34 -0700

    Start work on displaying async / await examples

--- src/AppWithAsync.tsx
+++ src/AppWithAsync.tsx
@@ -1,12 +1,21 @@
-import React, { FC, useEffect } from 'react'
+import React, { FC, useEffect, useState } from 'react'
 
 // utils
 import { IResolution } from './utils'
 
 // local
+import {
+
+} from './examples'
+
+import { Example } from './Example'
 import { SimpleAppContent } from './SimpleAppContent'
 
 import './App.css'
+
+const examples: Example[] = [
+
+]
 
 const AppWithAsync: FC<any> = () => {
 	useEffect(() => {
@@ -36,7 +45,61 @@ const AppWithAsync: FC<any> = () => {
 		.catch(rejectionReason => { console.error(`${token} promise rejected...`, JSON.stringify(rejectionReason, null, 4)) })
 	}, [])
 
-	return <SimpleAppContent pageTitle={'AppWithAsync (Examples using await / async keywords)'} />
+	const [currentExampleIndex, setCurrentExampleIndex] = useState(0)
+	const handleClickNext = () => {
+		setCurrentExampleIndex(oldIndex => (oldIndex + 1) % examples.length)
+	}
+	const handleClickPrevious = () => {
+		setCurrentExampleIndex(oldIndex => {
+			let newVal = oldIndex - 1
+
+			if (newVal < 0) {
+				newVal = examples.length - 1
+			}
+
+			return newVal
+		})
+	}
+	const handleClickRun = () => {
+		try {
+			/* eslint no-eval: 0 */
+			eval(examples[currentExampleIndex].text)
+		} catch (err) {
+			alert(err)
+		}
+	}
+	const currentExample = examples[currentExampleIndex]
+
+	return (
+		<SimpleAppContent pageTitle={'AppWithAsync (Examples using await / async keywords)'}>
+			<section className="btn-controls">
+				<button className="btn-change-example prev"
+					onClick={handleClickPrevious}
+					>
+					Previous Example
+				</button>
+				<button className="btn-change-example next"
+					onClick={handleClickNext}
+					>
+					Next Example
+				</button>
+			</section>
+			<h3>Example {currentExampleIndex + 1} - {currentExample.summary}</h3>
+			{currentExample.description.length > 0 && <h5>{currentExample.description}</h5>}
+			<textarea
+				cols={120}
+				readOnly={true}
+				rows={15}
+				style={{ margin: 25 }}
+				value={currentExample.text}
+			/>
+			<button className="btn-run-example"
+				onClick={handleClickRun}
+				>
+				Run Example
+			</button>
+		</SimpleAppContent>
+	)
 }
 
 export default AppWithAsync
