bd14c4d537068f86c2768bf752bf82b471ed016e
Anthony Williams <anthony.williams.cs@gmail.com>
2020-01-08 23:35:07 -0700

    Dynamically load Q library in example

--- src/examples.ts
+++ src/examples.ts
@@ -223,22 +223,29 @@ p.then(({ error, msg }) => {
 `
 
 export const Q_EXAMPLE_VALID =
-`// NOTE: VALID way to create resolved promise
-const p = Q.Promise((resolve, reject, notify) => { resolve({ msg: 'from resolve' }) })
+`// NOTE: make Q library available first by fetching over the web
+fetch('https://cdnjs.cloudflare.com/ajax/libs/q.js/1.5.1/q.js')
+	.then(resp => resp.text())
+	.then(respText => {
+		eval(respText) // NOTE: execute Q library code dynamically
 
-alert('promise created')
+		// NOTE: VALID way to create resolved promise
+		const p = Q.Promise((resolve, reject, notify) => { resolve({ msg: 'from resolve' }) })
 
-p.then(({ error, msg }) => {
-	if (error) {
-		throw new Error(error)
-	}
-	alert(\`inside then... resolution.msg=\${msg}...\`)
-})
-.progress(progress => {
-	console.info(\`progress=\${progress}\`)
-})
-.fail(err => {
-	alert(\`an error occured... \\n\\n\${ JSON.stringify(err, null, 4) }\`)
-})
-.done()
+		alert('promise created')
+
+		p.then(({ error, msg }) => {
+			if (error) {
+				throw new Error(error)
+			}
+			alert(\`inside then... resolution.msg=\${msg}...\`)
+		})
+		.progress(progress => {
+			console.info(\`progress=\${progress}\`)
+		})
+		.fail(err => {
+			alert(\`an error occured... \\n\\n\${ JSON.stringify(err, null, 4) }\`)
+		})
+		.done()
+	})
 `
