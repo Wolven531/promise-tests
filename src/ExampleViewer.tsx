2994d127ac60b8c14c3b63327d3c23627a70405f
Anthony Williams <anthony.williams.cs@gmail.com>
2020-01-10 20:56:04 -0700

    First pass on ExampleViewer

--- src/ExampleViewer.tsx
+++ src/ExampleViewer.tsx
@@ -1,11 +1,70 @@
-import React, { FC } from 'react'
+import React, { FC, useState } from 'react'
 
 // local
-// import { Example } from './Example'
-// import { SimpleAppContent } from './SimpleAppContent'
+import { Example } from './Example'
+import { SimpleAppContent } from './SimpleAppContent'
 
-const ExampleViewer: FC<any> = (props) => {
-    return (null)
+export interface IExampleViewerProps {
+	examples: Example[]
+	title: string
+}
+
+const ExampleViewer: FC<any> = props => {
+	const [currentExampleIndex, setCurrentExampleIndex] = useState(0)
+	const handleClickNext = () => {
+		setCurrentExampleIndex(oldIndex => (oldIndex + 1) % props.examples.length)
+	}
+	const handleClickPrevious = () => {
+		setCurrentExampleIndex(oldIndex => {
+			let newVal = oldIndex - 1
+
+			if (newVal < 0) {
+				newVal = props.examples.length - 1
+			}
+
+			return newVal
+		})
+	}
+	const handleClickRun = () => {
+		try {
+			/* eslint no-eval: 0 */
+			eval(props.examples[currentExampleIndex].text)
+		} catch (err) {
+			alert(err)
+		}
+	}
+	const currentExample = props.examples[currentExampleIndex]
+
+	return (
+		<SimpleAppContent pageTitle={props.title}>
+			<section className="btn-controls">
+				<button className="btn-change-example prev"
+					onClick={handleClickPrevious}
+					>
+					Previous Example
+				</button>
+				<button className="btn-change-example next"
+					onClick={handleClickNext}
+					>
+					Next Example
+				</button>
+			</section>
+			<h3>Example {currentExampleIndex + 1} - {currentExample.summary}</h3>
+			{currentExample.description.length > 0 && <h5>{currentExample.description}</h5>}
+			<textarea
+				cols={120}
+				readOnly={true}
+				rows={15}
+				style={{ margin: 25 }}
+				value={currentExample.text}
+			/>
+			<button className="btn-run-example"
+				onClick={handleClickRun}
+				>
+				Run Example
+			</button>
+		</SimpleAppContent>
+	)
 }
 
 export { ExampleViewer }
