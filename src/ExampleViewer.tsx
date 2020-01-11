a43aa4f759b6f59a95892c9d19cbe7794b101244
Anthony Williams <anthony.williams.cs@gmail.com>
2020-01-10 20:47:41 -0700

    Start work on ExampleViewer

---
+++ src/ExampleViewer.tsx
@@ -0,0 +1,11 @@
+import React, { FC } from 'react'
+
+// local
+// import { Example } from './Example'
+// import { SimpleAppContent } from './SimpleAppContent'
+
+const ExampleViewer: FC<any> = (props) => {
+    return (null)
+}
+
+export { ExampleViewer }
