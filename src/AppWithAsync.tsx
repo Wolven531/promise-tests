bbf3aeba486eb7234b8c47f23abad3e2d513df69
Anthony Williams <anthony.williams.cs@gmail.com>
2020-01-10 20:56:59 -0700

    Use ExampleViewer in AppWithAsync

--- src/AppWithAsync.tsx
+++ src/AppWithAsync.tsx
@@ -1,4 +1,4 @@
-import React, { FC, useState } from 'react'
+import React, { FC } from 'react'
 
 // local
 import {
@@ -10,7 +10,7 @@ import {
 } from './examples/examplesAsync'
 
 import { Example } from './Example'
-import { SimpleAppContent } from './SimpleAppContent'
+import { ExampleViewer } from './ExampleViewer'
 
 import './App.css'
 
@@ -35,61 +35,9 @@ const examples: Example[] = [
 ]
 
 const AppWithAsync: FC<any> = () => {
-	const [currentExampleIndex, setCurrentExampleIndex] = useState(0)
-	const handleClickNext = () => {
-		setCurrentExampleIndex(oldIndex => (oldIndex + 1) % examples.length)
-	}
-	const handleClickPrevious = () => {
-		setCurrentExampleIndex(oldIndex => {
-			let newVal = oldIndex - 1
-
-			if (newVal < 0) {
-				newVal = examples.length - 1
-			}
-
-			return newVal
-		})
-	}
-	const handleClickRun = () => {
-		try {
-			/* eslint no-eval: 0 */
-			eval(examples[currentExampleIndex].text)
-		} catch (err) {
-			alert(err)
-		}
-	}
-	const currentExample = examples[currentExampleIndex]
-
-	return (
-		<SimpleAppContent pageTitle={'AppWithAsync (Examples using await / async keywords)'}>
-			<section className="btn-controls">
-				<button className="btn-change-example prev"
-					onClick={handleClickPrevious}
-					>
-					Previous Example
-				</button>
-				<button className="btn-change-example next"
-					onClick={handleClickNext}
-					>
-					Next Example
-				</button>
-			</section>
-			<h3>Example {currentExampleIndex + 1} - {currentExample.summary}</h3>
-			{currentExample.description.length > 0 && <h5>{currentExample.description}</h5>}
-			<textarea
-				cols={120}
-				readOnly={true}
-				rows={15}
-				style={{ margin: 25 }}
-				value={currentExample.text}
-			/>
-			<button className="btn-run-example"
-				onClick={handleClickRun}
-				>
-				Run Example
-			</button>
-		</SimpleAppContent>
-	)
+	return <ExampleViewer
+		examples={examples}
+		title={'AppWithAsync (Examples using await / async keywords)'} />
 }
 
 export default AppWithAsync
