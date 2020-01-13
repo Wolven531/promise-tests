a2495b0bcd18d90ad8ede6fb289ccafbb5dfd260
Anthony Williams <anthony.williams.cs@gmail.com>
2020-01-12 19:47:29 -0700

    Fix mobile display - pass 1

--- src/ExampleViewer.tsx
+++ src/ExampleViewer.tsx
@@ -4,6 +4,8 @@ import React, { FC, useState } from 'react'
 import { Example } from './Example'
 import { SimpleAppContent } from './SimpleAppContent'
 
+import './ExampleViewer.css'
+
 export interface IExampleViewerProps {
 	examples: Example[]
 	title: string
@@ -36,34 +38,38 @@ const ExampleViewer: FC<any> = props => {
 	const currentExample = props.examples[currentExampleIndex]
 
 	return (
-		<SimpleAppContent pageTitle={props.title}>
-			<section className="btn-controls">
-				<button className="btn-change-example prev"
-					onClick={handleClickPrevious}
+		<article className="example-viewer">
+			<SimpleAppContent pageTitle={props.title}>
+				<section className="btn-controls">
+					<button className="btn-change-example prev"
+						onClick={handleClickPrevious}
+						>
+						Previous Example
+					</button>
+					<button className="btn-change-example next"
+						onClick={handleClickNext}
+						>
+						Next Example
+					</button>
+				</section>
+				<h3>Example {currentExampleIndex + 1} - {currentExample.summary}</h3>
+				{currentExample.description.length > 0 && <h5>{currentExample.description}</h5>}
+				<textarea
+					className="example-txt"
+					cols={52}
+					readOnly={true}
+					rows={15}
+					// value={currentExample.text}
+					wrap="soft"
+					// wrap="hard"
+				>{currentExample.text}</textarea>
+				<button className="btn-run-example"
+					onClick={handleClickRun}
 					>
-					Previous Example
+					Run Example
 				</button>
-				<button className="btn-change-example next"
-					onClick={handleClickNext}
-					>
-					Next Example
-				</button>
-			</section>
-			<h3>Example {currentExampleIndex + 1} - {currentExample.summary}</h3>
-			{currentExample.description.length > 0 && <h5>{currentExample.description}</h5>}
-			<textarea
-				cols={120}
-				readOnly={true}
-				rows={15}
-				style={{ margin: 25 }}
-				value={currentExample.text}
-			/>
-			<button className="btn-run-example"
-				onClick={handleClickRun}
-				>
-				Run Example
-			</button>
-		</SimpleAppContent>
+			</SimpleAppContent>
+		</article>
 	)
 }
 
